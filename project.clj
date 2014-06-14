99321753d59a895b9c1a6a4cfa027b90ab43815e
bakpakin <calsrose@gmail.com>
2014-06-14 08:58:27 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ezglib "0.1.1"
+(defproject ezglib "0.1.2"
   :description "An easy game library for ClojureScript."
   :url "https://github.com/bakpakin/ezglib"
   :license {:name "Eclipse Public License"
