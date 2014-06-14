ded7bae63d8fa563cd6ef88d6c2c1dc9beb05ae4
bakpakin <calsrose@gmail.com>
2014-06-14 09:47:53 -0400

    Remove build js.

--- examples/js/cljs.js
+++
@@ -1,4 +0,0 @@
-goog.addDependency("base.js", ['goog'], []);
-goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.array', 'goog.object', 'goog.string.StringBuffer']);
-goog.addDependency("../ezglib/core.js", ['ezglib.core'], ['cljs.core']);
-goog.addDependency("../basic/core.js", ['basic.core'], ['cljs.core', 'ezglib.core']);
