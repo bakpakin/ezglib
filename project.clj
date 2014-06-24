c3b1cdaa3a91d48c1d1694f2d68404f5103dd228
bakpakin <calsrose@gmail.com>
2014-06-23 21:22:46 -0400

    Change project version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ezglib "0.1.2-SNAPSHOT"
+(defproject ezglib "0.1.3-SNAPSHOT"
   :description "An easy game library for ClojureScript."
   :url "https://github.com/bakpakin/ezglib"
   :license {:name "Eclipse Public License"
---
+++ src/ezglib/math.cljs
@@ -0,0 +1,3 @@
+(ns ezglib.math)
+
+()
