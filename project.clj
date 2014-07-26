81209aa585dce0d4470e4570af8992d351e4127d
bakpakin <calsrose@gmail.com>
2014-07-26 17:00:55 -0400

    Change project.clj

--- project.clj
+++ project.clj
@@ -5,18 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
          :url "https://github.com/bakpakin/ezglib"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2268"]]
-  :plugins [[lein-cljsbuild "1.0.3"]]
-  :hooks [leiningen.cljsbuild]
-  :source-paths ["src"]
-  :cljsbuild {
-    :builds {
-      :examples {
-        :incremental? true
-        :source-paths ["src" "examples/src"]
-        :compiler {:output-to "examples/js/cljs.js"
-                   :output-dir "examples/js"
-                   :optimizations :none
-                   :pretty-print true
-                   :source-map "examples/js/cljs.js.map"}}}})
+  :source-paths ["src"])
