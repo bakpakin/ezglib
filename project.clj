7243beabba8beb6524d99ecd1e82504b07f6221c
bakpakin <calsrose@gmail.com>
2014-08-13 11:29:57 -0400

    Add clojurescript dependency to project.clj

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject ezglib "0.0.0-SNAPSHOT"
   :description "Make WebGL games in ClojureScript."
   :url "https://github.com/bakpakin/ezglib"
-  :dependencies [[tailrecursion/cljs-priority-map "1.1.0"]]
+  :dependencies [[org.clojure/clojurescript "0.0-2280"]
+                 [tailrecursion/cljs-priority-map "1.1.0"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
         :url "https://github.com/bakpakin/ezglib"}
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
-                                  [org.clojure/clojurescript "0.0-2280"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]
                    :hooks [leiningen.cljsbuild]
                    :plugins [[codox "0.8.10"]
                              [lein-cljsbuild "1.0.3"]]}}
